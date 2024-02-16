121669e343d2af9ec57a97b0772fdb7ebd2362e3
2paperstar <lee@paperst.ar>
2024-02-16 10:17:54 +0900

    feat: login with kakao

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -9,15 +10,29 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  late InAppWebViewController _controller;
-  static const String _url = 'http://localhost:5173/Login';
+  static const String _url = 'http://localhost:5173/login';
+
+  Future<OAuthToken> _getToken() async {
+    if (await isKakaoTalkInstalled()) {
+      return await UserApi.instance.loginWithKakaoTalk();
+    }
+    return await UserApi.instance.loginWithKakaoAccount();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: InAppWebView(
-          onWebViewCreated: (controller) => _controller = controller,
+          onWebViewCreated: (controller) {
+            controller.addJavaScriptHandler(
+                handlerName: 'loginWithKakao',
+                callback: (args) async {
+                  final token = await _getToken();
+                  return token.idToken;
+                });
+          },
+          onConsoleMessage: (_, message) => print(message),
           initialUrlRequest: URLRequest(url: Uri.parse(_url)),
           initialOptions: InAppWebViewGroupOptions(
             ios: IOSInAppWebViewOptions(
